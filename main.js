b3498bba4a9b444cb96f9b00417e093b03413af2
zara-khan123 <106547199+zara-khan123@users.noreply.github.com>
2023-01-21 16:39:49 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -34,34 +34,46 @@ function speak()
     synth.speak(utterThis);
 }
 
+function check()
+{
+    img = document.getElementById('capture');
+    classifier.classify(img, gotResult);
+}
 
+function gotResult(error, results) {
+    if (error) {
+     console.error(error);
+    } else {
+       console.log(results);
+       document.getElementById("result_emotion_name").innerHTML = results[0].label;
+       document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+   prediction_1 = results[0].label;
+   prediction_2 = results[1].label;
+   speak();
+   if(results[0].label == "happy")
+    {
+        document.getElementById("update_emoji").innerHTML = "&#128522";
+    }
+    if(results[0].label == "sad")
+    {
+        document.getElementById("update_emoji").innerHTML = "&#128532";
+    }
+    if(results[0].label == "angry")
+    {
+        document.getElementById("update_emoji").innerHTML = "&#128548";
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if(results[1].label == "happy")
+    {
+        document.getElementById("update_emoji2").innerHTML = "&#128522";
+    }
+    if(results[1].label == "sad")
+    {
+        document.getElementById("update_emoji2").innerHTML = "&#128532";
+    }
+    if(results[1].label == "angry")
+    {
+        document.getElementById("update_emoji2").innerHTML = "&#128548";
+    }
+}
+}
